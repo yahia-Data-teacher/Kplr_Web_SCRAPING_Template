7ac46b58934fd38bb63a93b770f3ee4d7af4ed9b
yahia-kplr <yahia.maamar@kplr.fr>
2023-03-10 10:53:28 +0000

    commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import json
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--disable-extensions")
+
+driver = webdriver.Chrome(options=chrome_options)
+
+# Telechager les fichier csv "vin_11"
+
+# ouvrir le URL
+driver.get("https:/google.com")
+
+##  Finding Elements
+
+google_text = driver.find_element(By.CLASS_NAME, "MV3Tnb").text
+
+print(google_text)
+
+input_box = driver.find_element(By.NAME, "q")
+
+## Typing and Clicking
+
+input_box.send_keys("Hi")
+
+input_box.send_keys(Keys.ENTER)
+
+home_link = driver.find_element(By.ID, "logo")
+
+home_link.click()
+
+## Selectors
+
+tag_search = driver.find_element(By.TAG_NAME, "a").text
+
+print(tag_search)
+
+link_text = driver.find_element(By.LINK_TEXT, "About").text
+
+print(link_text)
